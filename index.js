569cac7996eef74c9dd417991a4449a2561cae13
Shruti Jain <jainshruti0512@gmail.com>
2024-03-15 15:31:29 +0530

    Details of styling

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-let btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {
-    alert("Hello , World!!")
-})
+// Function to show alert
+function showPopup() {
+    document.getElementById('alert').style.display = "block";
+}
+
+// Function to close alert
+function closePopup() {
+    document.getElementById('alert').style.display = "none";
+}
